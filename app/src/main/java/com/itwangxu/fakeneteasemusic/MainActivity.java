088e0f2e3ed76f6b8d22b930d0bb765690451155
wangxu95 <wangxu95@wanda.cn>
2016-12-16 17:47:01 +0800

    init toolbar

--- app/src/main/java/com/itwangxu/fakeneteasemusic/MainActivity.java
+++ app/src/main/java/com/itwangxu/fakeneteasemusic/MainActivity.java
@@ -1,45 +1,77 @@
 package com.itwangxu.fakeneteasemusic;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+    implements
+      NavigationView.OnNavigationItemSelectedListener,View.OnClickListener {
+
+  private DrawerLayout drawer;
+  private Toolbar toolbar;
+  private ImageButton discoverBtn;
+  private ImageButton musicBtn;
+  private ImageButton friendsBtn;
+  private FloatingActionButton fab;
+  private NavigationView navigationView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
+    initActionBar(R.id.toolbar);
+    initDrawer();
+    initViews();
+    intListener();
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+  }
+
+  private void initViews() {
+    fab = (FloatingActionButton) findViewById(R.id.fab);
+
+    navigationView = (NavigationView) findViewById(R.id.nav_view);
+  }
+
+  private void intListener() {
+    navigationView.setNavigationItemSelectedListener(this);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+            .setAction("Action", null).show();
       }
     });
+    discoverBtn.setOnClickListener(this);
+    friendsBtn.setOnClickListener(this);
+    musicBtn.setOnClickListener(this);
+  }
 
-    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+  private void initDrawer() {
+    drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
     drawer.setDrawerListener(toggle);
     toggle.syncState();
+  }
 
-    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-    navigationView.setNavigationItemSelectedListener(this);
+  private void initActionBar(@IdRes int toolbarId) {
+    toolbar = (Toolbar) findViewById(toolbarId);
+    discoverBtn = (ImageButton) toolbar.findViewById(R.id.btn_discover);
+    musicBtn = (ImageButton) toolbar.findViewById(R.id.btn_music);
+    friendsBtn = (ImageButton) toolbar.findViewById(R.id.btn_friends);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayShowTitleEnabled(false);
   }
 
   @Override
@@ -52,27 +84,6 @@ public class MainActivity extends AppCompatActivity
     }
   }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
 
   @SuppressWarnings("StatementWithEmptyBody")
   @Override
@@ -97,5 +108,10 @@ public class MainActivity extends AppCompatActivity
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     drawer.closeDrawer(GravityCompat.START);
     return true;
+  }
+
+  @Override
+  public void onClick(View view) {
+
   }
 }
